d3c37a2d1e3223320ad955c27ef18728298c7b55
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2026-04-01 22:46:51 -0500

    feat: version anterior del backend

--- Rural_House/Rural_House/src/main/java/co/uniquindio/rural_house/Rural_House/dto/request/PhotoRequest.java
+++ Rural_House/Rural_House/src/main/java/co/uniquindio/rural_house/Rural_House/dto/request/PhotoRequest.java
@@ -1,10 +1,9 @@
 package co.uniquindio.rural_house.Rural_House.dto.request;
 
 import lombok.Data;
-import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class PhotoRequest {
-    private MultipartFile file;
+    private String url;
     private String description;
 }
--- Rural_House/Rural_House/src/main/java/co/uniquindio/rural_house/Rural_House/entity/Photo.java
+++ Rural_House/Rural_House/src/main/java/co/uniquindio/rural_house/Rural_House/entity/Photo.java
@@ -18,9 +18,6 @@ public class Photo {
     @Column(nullable = false)
     private String url;
 
-    @Column(nullable = false)
-    private String fileName;
-
     private String description;
 
     @ManyToOne(fetch = FetchType.LAZY)
